33815203b4740bc41d8c2328007d72b6be4a1337
PdroKa <kauanpedroreis4@gmail.com>
2025-06-04 17:56:25 -0300

    SEO: Continue Config seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { SmoothScrollToHash } from "@/lib/SmoothScrollToHash";
 import { Roboto } from "next/font/google";
 import { siteConfig } from "@/config/site";
 
+
 const roboto = Roboto({
   subsets: ["latin"],
   weight: ["400", "500", "700"]
@@ -12,30 +13,33 @@ const roboto = Roboto({
 
 export const metadata: Metadata = {
   title: {
-    default: siteConfig.name,
-    template: `%s | ${siteConfig.name}`
+    default: siteConfig.title,
+    template: `%s | ${siteConfig.title}`
   },
+  keywords: siteConfig.keywords,
   description: siteConfig.description,
   metadataBase: new URL(siteConfig.url),
+
   openGraph: {
-    title: siteConfig.name,
+    title: siteConfig.title,
     description: siteConfig.description,
     url: siteConfig.url,
     siteName: siteConfig.name,
     images: [
       {
         url: siteConfig.ogImage,
-        width: 1200,
-        height: 630,
-        alt: `${siteConfig.name} Open Graph Image`,
+        width: 1980,
+        height: 1320,
+        alt: `${siteConfig.title} - ${siteConfig.name} `,
       },
+
     ],
     locale: "pt_BR",
     type: "website",
   },
   twitter: {
     card: "summary_large_image",
-    title: siteConfig.name,
+    title: siteConfig.title,
     description: siteConfig.description,
     images: [siteConfig.ogImage],
   },
--- src/config/site.ts
+++ src/config/site.ts
@@ -1,5 +1,6 @@
 type SiteConfig = {
     name: string
+    title: string
     description: string
     url: string
     ogImage: string
@@ -10,10 +11,12 @@ type SiteConfig = {
         youtube?: string
         suporte?: string
     }
+    keywords?: string[]
 }
 
 export const siteConfig: SiteConfig = {
-    name: 'Adorai Curitiba 2025 | 12 de Outubro',
+    name: 'Com Geração',
+    title: 'Adorai Curitiba 2025 | 12 de Outubro',
     description: 'Retiro de 2 dias com momentos de adoração, oração e unidade. Garanta seu ingresso na pré-venda e viva essa experiência única com Deus.',
     url: 'https://comgeracao.com.br/adoraicuritiba',
     ogImage: 'https://comgeracao.com.br/adoraicuritiba/og-image.avif',
@@ -24,4 +27,16 @@ export const siteConfig: SiteConfig = {
         youtube: "https://youtube.com/@adorai.curitiba",
         suporte: "https://wa.me/5541999999999"
     },
+    keywords: [
+        "Adorai",
+        "Adorai Curitiba",
+        "Retiro espiritual",
+        "Retiro católico",
+        "Adoração",
+        "Evento Curitiba",
+        "Adoradores",
+        "12 de Outubro",
+        "Verdadeiros adoradores",
+        "Jovens adoradores",
+    ],
 }
