7359688fd8d31b39f608fc29a698e89f205e803b
PdroKa <kauanpedroreis4@gmail.com>
2025-06-16 13:12:32 -0300

    Fix: Fixed Bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import "./globals.css";
 import { Roboto } from "next/font/google";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { SmoothScrollToHash } from "@/app/lib/SmoothScrollToHash";
-import { Link } from "lucide-react";
-import Script from "next/script";
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -17,8 +15,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <Script rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css"/>
-
         <body
           cz-shortcut-listen="true"
           className={`${roboto.className} bg-background text-white antialiased scroll-smooth`}>
