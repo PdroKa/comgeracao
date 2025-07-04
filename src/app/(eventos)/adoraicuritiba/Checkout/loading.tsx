068baccd84449f3bd7b00b878d09c40075ad9440
PdroKa <kauanpedroreis4@gmail.com>
2025-07-04 17:01:52 -0300

    refact: refatorado nome de pastas para organizacao

--- src/app/(eventos)/adoraicuritiba/Checkout/loading.tsx
+++
@@ -1,8 +0,0 @@
-import { LoadingSpinner } from '@/components/ui/loadingSpinner';
-export default function Loading() {
-    // You can add any UI inside Loading, including a Skeleton.
-    return (
-        <div className='h-screen flex items-center justify-center'>
-            <LoadingSpinner/>
-        </div>)
-}
