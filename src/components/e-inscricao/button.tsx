36e548b8e3dc262bb67a4f71245311574b9be80b
PdroKa <kauanpedroreis4@gmail.com>
2025-08-19 10:06:40 -0300

    env: adicionado consumo de URL em variavel

--- src/components/e-inscricao/button.tsx
+++ src/components/e-inscricao/button.tsx
@@ -15,21 +15,20 @@ export function ButtonEinscricao({ children, className, size, ...props }: {
             import('@e-inscricao/widget'!)
         }
     }, [])
-
     return (
         <Button
             {...props}
             size={size}
             className={`w-full text-base font-semibold ${className}`}
-            data-einscricao-event="97574"
-            data-einscricao-url="https://checkout.einscricao.app/"
-            data-einscricao-api-url="https://www.e-inscricao.com"
-            data-einscricao-theme-color-primary="#000"
-            data-einscricao-theme-color-secondary="#dcbe59"
-            data-einscricao-ms-api-url="https://ei-pay.e-inscricao.com"
-            data-einscricao-receipt-url="https://recibo.e-inscricao.tech"
-            data-einscricao-theme-logo="https://comgeracao.com.br/Logo-remove-240.png"
-            data-einscricao-api-participants-url="https://participants.e-inscricao.com"
+            data-einscricao-event={process.env.NEXT_PUBLIC_EINSCRICAO_EVENT}
+            data-einscricao-url={process.env.NEXT_PUBLIC_EINSCRICAO_URL}
+            data-einscricao-api-url={process.env.NEXT_PUBLIC_EINSCRICAO_API_URL}
+            data-einscricao-theme-color-primary={'#000'}
+            data-einscricao-theme-color-secondary={'#dcbe59'}
+            data-einscricao-ms-api-url={process.env.NEXT_PUBLIC_EINSCRICAO_MS_API_URL}
+            data-einscricao-receipt-url={process.env.NEXT_PUBLIC_EINSCRICAO_RECEIPT_URL}
+            data-einscricao-theme-logo={process.env.NEXT_PUBLIC_EINSCRICAO_THEME_LOGO}
+            data-einscricao-api-participants-url={process.env.NEXT_PUBLIC_EINSCRICAO_API_PARTICIPANTS_URL}
             aria-label="Realizar inscrição no evento Adorai Curitiba"
             type="button">
             {children}
